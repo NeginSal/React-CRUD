82966432cf1e19cc8db0fa6b1635459ee913840a
NeginSal <neginsaljooghi@gmail.com>
2022-04-10 17:25:23 +0430

    add router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddUser from './components/AddUser';
 import EditUser from './components/EditUser';
 import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      <AddUser/>
-      <EditUser/>
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="add" element={<AddUser />}></Route>
+          <Route path="/edit/:id" element={<EditUser />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
