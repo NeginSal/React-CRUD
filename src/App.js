7e4223bb795aaf1811bd494f7acfeef45e1a8421
NeginSal <neginsaljooghi@gmail.com>
2022-04-11 16:39:09 +0430

    edit add user component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddUser from './components/AddUser';
 import EditUser from './components/EditUser';
-import Home from './components/Home'
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div style={{maxWidth:'30rem',margin:'4rem auto'}}>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="add" element={<AddUser />}></Route>
--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,9 +1,23 @@
+import { Link } from 'react-router-dom';
+import {
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Button
+} from 'reactstrap'
 const AddUser = () => {
-    return ( 
-        <div>
-            <h1>AddUser</h1>
-        </div>
-     );
+    return (
+        <Form>
+            <FormGroup>
+                <label>Name</label>
+                <Input type='text' palaceholder="Enter Name"></Input>
+            </FormGroup>
+            <Button type='submit'className='bg-success '>Submit</Button>
+            <Link to="/" className="btn btn-danger m-2">Cancel</Link>
+           
+        </Form>
+    );
 }
- 
+
 export default AddUser;
