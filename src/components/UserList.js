117bfff9a234b09ed6f0fce462a3180de1c166d3
NeginSal <neginsaljooghi@gmail.com>
2022-04-12 19:18:37 +0430

    edit userList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,10 +1,29 @@
 import { Link } from 'react-router-dom'
+import {
+    ListGroup,
+    ListGroupItem,
+    Button
+} from 'reactstrap'
 
 const UserList = () => {
     return (
-        <div>
-            <h1>UserList</h1>
-        </div>
+        <ListGroup className='mt-4'>
+            <ListGroupItem className='d-flex'>
+                <strong>User One</strong>
+                <div className='ms-auto'>
+                    <Link className='btn btn-warning me-1' to='/edit/1'>Edit</Link>
+                    <Button color='danger'>Delete</Button>
+                </div>
+            </ListGroupItem>
+            <ListGroupItem className='d-flex'>
+                <strong>User One</strong>
+                <div className='ms-auto'>
+                    <Link className='btn btn-warning me-1' to='/edit/2'>Edit</Link>
+                    <Button color='danger'>Delete</Button>
+                </div>
+            </ListGroupItem>
+
+        </ListGroup>
     );
 }
 
