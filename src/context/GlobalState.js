fc9349a39d50765941b68aa1559e1d032d873e43
NeginSal <neginsaljooghi@gmail.com>
2022-05-08 16:51:26 +0430

    add Global state & appReducer

---
+++ src/context/GlobalState.js
@@ -0,0 +1,24 @@
+import React, { createContext, useReducer } from 'react'
+import AppReducer from './AppReducer'
+
+//Initial State
+const initialState = {
+    users: [
+        { id: 1, name: userOne },
+        { id: 2, name: userTwo },
+        { id: 3, name: userThree },
+    ]
+}
+
+//Create Context
+export const GlobalContext = createContext(initialState);
+
+//Provider Component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+    return(
+        <GlobalContext.Provider value={{users:state.users}}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
